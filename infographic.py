8464919348e041e55472bbfcc5bd351af426e8dd
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2013-10-30 17:43:55 -0400

    wordcloud

--- infographic.py
+++ infographic.py
@@ -1,14 +1,16 @@
 import csv
 import json
+import codecs
 
 def write_to_files(csv_file):
-  with open(csv_file, 'rb') as infile, open('boilerplate_text.txt', 'wb') as boil, open('titles.txt','wb') as titles: 
+  with codecs.open(csv_file, 'rb') as infile, codecs.open('boilerplate_text.txt', 'w','utf-8') as boil, codecs.open('titles.txt','w','utf-8') as titles: 
     reader = csv.reader(infile, delimiter = '\t')
     headers = reader.next()
     for line in reader:
       boiler = json.loads(line[2])
-      titles.write("%s\n" % boiler['title'])
-      boil.write("%s\n" % boiler['body'])
+      # import ipdb; ipdb.set_trace()
+      titles.write("%s\n" % unicode(boiler.get('title','')))
+      boil.write("%s\n" % unicode(boiler.get('body','')))
 
 if __name__ == '__main__':
   write_to_files('train.tsv')
